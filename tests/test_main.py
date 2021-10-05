ffcf0fa25df87357fe13c37ea9ae4b2729361c30
nader <nado0or97@hotmail.com>
2021-10-05 21:57:03 +0200

    rewrite tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,32 +11,33 @@ def test_meta_validation():
     assert main.meta_validation("log-poison") == True
 
 
-def test_add_module_ssh():
+def test_add_module_no_dependencies():
 
     assert main.add_module("ssh") == True
     assert main.install_list == ["ssh"]
     assert main.dependencies_list == ["ssh"]
 
 
-def test_add_module_dre():
+def test_add_module_unsatisfied():
     assert main.add_module("dre") == False
     assert main.install_list == ["ssh"]
     assert main.dependencies_list == ["ssh"]
 
 
-def test_add_module_log_poison():
-    assert main.add_module("log-poison") == True
-    assert main.install_list == ["ssh", "log-poison"]
-    assert main.dependencies_list == ["ssh", "php"]
-
-
-def test_add_module_ssh_userpass():
-    assert main.add_module("ssh-userpass") == True
-    assert main.install_list == ["ssh", "log-poison", "ssh-userpass"]
-    assert main.dependencies_list == ["ssh", "php"]
-
-
-def test_add_module_lemp():
+def test_add_module_satisfied():
     assert main.add_module("lemp") == True
-    assert main.install_list == ["ssh", "log-poison", "ssh-userpass", "lemp"]
-    assert main.dependencies_list == ["ssh", "php", "php-fpm", "mysql", "nginx", "patch"]
+    assert main.add_module("dre") == True
+    assert main.install_list == ["ssh", "lemp", "dre"]
+    assert main.dependencies_list == ["ssh", "php-fpm", "mysql", "nginx", "patch", "php-gnupg"]
+
+
+def test_add_module_log_poison_satisfied():
+    assert main.add_module("log-poison") == True
+    assert main.install_list == ["ssh", "lemp", "dre", "log-poison"]
+    assert main.dependencies_list == ["ssh", "php-fpm", "mysql", "nginx", "patch", "php-gnupg", "php"]
+
+
+def test_add_module_ssh_userpass_satisfied():
+    assert main.add_module("ssh-userpass") == True
+    assert main.install_list == ["ssh", "lemp", "dre", "log-poison", "ssh-userpass"]
+    assert main.dependencies_list == ["ssh", "php-fpm", "mysql", "nginx", "patch", "php-gnupg", "php"]
